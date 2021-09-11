e2f3693424d9295d028d74e116b9889398da68f4
Darrel Daley <darreldaley@protonmail.com>
2021-09-10 20:33:48 -0400

    finished section 54

--- seeds/index.js
+++ seeds/index.js
@@ -28,9 +28,22 @@ const seedDB = async () => {
             author: '61394e24747b88844312980a',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: '  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore unde expedita aliquid maxime excepturi eligendi cupiditate voluptatem veritatis nisi praesentium, accusamus doloribus ipsam, exercitationem laborum odio beatae provident. Sunt, quae?',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dxgnainiy/image/upload/v1631291715/YelpCamp/hwhm41hgf5baebmjhbni.jpg',
+                    filename: 'YelpCamp/hwhm41hgf5baebmjhbni'
+                  },
+                  {
+                    url: 'https://res.cloudinary.com/dxgnainiy/image/upload/v1631291715/YelpCamp/zubjyg46xu2kdy3nyvxq.jpg',
+                    filename: 'YelpCamp/zubjyg46xu2kdy3nyvxq'
+                  },
+                  {
+                    url: 'https://res.cloudinary.com/dxgnainiy/image/upload/v1631291715/YelpCamp/sigisnvdadntjolsxdx5.jpg',
+                    filename: 'YelpCamp/sigisnvdadntjolsxdx5'
+                  }
+            ]
         })
         await camp.save();
     }
